777c7a65de28404dd0d28c23d4d155f32e7602c8
SebaDL <jsdilauro@gmail.com>
2023-11-02 13:58:28 -0300

    ft: sdk protocol squad, move ui to right-bottom

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 export * from '@dcl/sdk'
 import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
-import { ui } from '../dae-test-tool/camera-mode/ui'
+import { setupUi } from '../dae-test-tool/camera-mode/ui'
 
-ReactEcsRenderer.setUiRenderer(ui)
+export function main() {
+    setupUi()
+}
+
+
