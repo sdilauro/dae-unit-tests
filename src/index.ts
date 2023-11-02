66633bf6f18f27db2dbbb0a5ead02073229e9b58
SebaDL <jsdilauro@gmail.com>
2023-11-02 17:43:18 -0300

    feat: Update readme

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 export * from '@dcl/sdk'
 import { setupUi } from '../tests/camera-mode/ui'
-import './index.test'
+import '../tests/camera-mode/index.test'
 
 export function main() {
     setupUi()
-
 }
 
 
