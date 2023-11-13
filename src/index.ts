bc52816c56e9064e628ded0fcf34c9e5095c5205
Seba Di Lauro <jsdilauro@gmail.com>
2023-11-13 20:00:54 -0300

    uncomment camera-mode-area test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { setupUi } from './tests/camera-mode/ui'
 import './tests/transform/index.test'
-// import './tests/camera-mode/index.test'
+import './tests/camera-mode/index.test'
 
 export function main(): void {
   setupUi()
