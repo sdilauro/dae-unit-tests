7d544e5a4e24ced808ddcd08dfdb737bbc60080f
Sebastián Di Lauro <jsdilauro@gmail.com>
2023-11-17 11:02:04 -0300

    Update src/index.ts
    Co-authored-by: Lean Mendoza <leandro@decentraland.org>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { setupUi } from './tests/camera-mode/ui'
-// import './tests/transform/index.test'
-// import './tests/camera-mode/index.test'
+import './tests/transform/index.test'
+import './tests/camera-mode/index.test'
 import './tests/raycast/index.test'
 
 export function main(): void {
