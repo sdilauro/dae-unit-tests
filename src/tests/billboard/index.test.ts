36c924f8337ce95a3085774aa00f9d86ea049ce5
Seba Di Lauro <jsdilauro@gmail.com>
2023-11-17 13:18:26 -0300

    frist commit add a simple test

---
+++ src/tests/billboard/index.test.ts
@@ -0,0 +1,85 @@
+import {
+  Billboard,
+  CameraModeArea,
+  EngineInfo,
+  MeshCollider,
+  MeshRenderer,
+  Raycast,
+  RaycastQueryType,
+  RaycastResult,
+  Transform,
+  engine
+} from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
+import { assertComponentValue, assertEquals } from '../../testing/assert'
+import { test } from './../../testing'
+import { customAddEntity } from './../../utils/entity'
+import { waitTicks } from './../../utils/waiters'
+import {
+  assertMovePlayerTo,
+  createChainedEntities
+} from './../../utils/helpers'
+
+const sceneCenter = Vector3.create(8, 1, 8)
+
+test('billboard: billboard follows the camera', function* (context) {
+  customAddEntity.clean()
+  //
+  const cameraModeAreaE = customAddEntity.addEntity()
+  Transform.create(cameraModeAreaE, { position: sceneCenter })
+  CameraModeArea.create(cameraModeAreaE, {
+    area: Vector3.create(16, 5, 16),
+    mode: 0
+  })
+
+  const billboardEntity = customAddEntity.addEntity()
+  Transform.create(billboardEntity, { position: sceneCenter })
+  Billboard.create(billboardEntity)
+
+  const colliderToRaycast = customAddEntity.addEntity()
+  Transform.create(colliderToRaycast, {
+    parent: engine.PlayerEntity,
+    position: Vector3.create(0, 2, 1),
+    scale: Vector3.create(1, 2, 0.1)
+  })
+
+  MeshRenderer.create(colliderToRaycast, {
+    mesh: {
+      $case: 'box',
+      box: { uvs: [] }
+    }
+  })
+  MeshCollider.create(colliderToRaycast, {
+    collisionMask: 1,
+    mesh: {
+      $case: 'box',
+      box: { uvs: [] }
+    }
+  })
+
+  yield* assertMovePlayerTo(Vector3.create(8, 0, 0), sceneCenter)
+
+  Raycast.create(billboardEntity, {
+    direction: {
+      $case: 'localDirection',
+      localDirection: Vector3.Backward()
+    },
+    maxDistance: 30,
+    queryType: RaycastQueryType.RQT_QUERY_ALL,
+    continuous: false,
+    collisionMask: 1
+  })
+
+  yield* waitTicks(3)
+
+  const rayResult = RaycastResult.get(billboardEntity)
+  // console.log({ rayResult })
+  // const playerPosition = Transform.get(engine.PlayerEntity).position
+  // console.log({ playerPosition })
+
+  assertEquals(
+    rayResult.hits.length,
+    1,
+    'raycast from billboarded entity should always hit the collider'
+  )
+})
