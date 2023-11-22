9d77ed2a3275878f443192c958e699245b5ad250
Seba Di Lauro <jsdilauro@gmail.com>
2023-11-22 18:15:53 -0300

    add basic engineInfo test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { setupUi } from './tests/camera-mode/ui'
-import './tests/transform/index.test'
-import './tests/camera-mode/index.test'
-import './tests/raycast/index.test'
-
-export function main(): void {
-  setupUi()
-}
+// import { setupUi } from './tests/camera-mode/ui'
+// import './tests/transform/index.test'
+// import './tests/camera-mode/index.test'
+// import './tests/raycast/index.test'
+import './tests/engine-info/index.test'
+export function main(): void {}
