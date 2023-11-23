683f0f4c50e28bbe390e32f9406ed22fe7fa409b
Seba Di Lauro <jsdilauro@gmail.com>
2023-11-23 14:55:13 -0300

    add pointer-lock basic test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { setupUi } from './tests/camera-mode/ui'
-import './tests/transform/index.test'
-import './tests/camera-mode/index.test'
-import './tests/raycast/index.test'
+// import { setupUi } from './tests/camera-mode/ui'
+// import './tests/transform/index.test'
+// import './tests/camera-mode/index.test'
+// import './tests/raycast/index.test'
+import './tests/pointer-lock/index.test'
 
 export function main(): void {
-  setupUi()
+  // setupUi()
 }
