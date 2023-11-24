777e99814d149b9971e05809de3dcecfeb8833d6
Seba Di Lauro <jsdilauro@gmail.com>
2023-11-24 15:34:08 -0300

    add basic visibility test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import { setupUi } from './tests/camera-mode/ui'
-import './tests/transform/index.test'
-import './tests/camera-mode/index.test'
-import './tests/raycast/index.test'
+// import { setupUi } from './tests/camera-mode/ui'
+// import './tests/transform/index.test'
+// import './tests/camera-mode/index.test'
+// import './tests/raycast/index.test'
+
+import { createBlackRoom } from './utils/black-room'
+import './tests/visibility/index.test'
 
 export function main(): void {
-  setupUi()
+  createBlackRoom()
 }
---
+++ src/utils/black-room.ts
@@ -0,0 +1,67 @@
+import {
+  CameraModeArea,
+  Entity,
+  Material,
+  MeshRenderer,
+  Transform,
+  engine
+} from '@dcl/sdk/ecs'
+import { Color4, Quaternion, Vector3 } from '@dcl/sdk/math'
+
+function createWall(position: Vector3, rotation: Quaternion): Entity {
+  const wall: Entity = engine.addEntity()
+  Transform.create(wall, {
+    position,
+    rotation,
+    scale: Vector3.create(15.5, 0.1, 15.5)
+  })
+  MeshRenderer.create(wall, {
+    mesh: {
+      $case: 'box',
+      box: { uvs: [] }
+    }
+  })
+
+  Material.setPbrMaterial(wall, {
+    albedoColor: Color4.Black(),
+    metallic: 1,
+    roughness: 1
+  })
+
+  return wall
+}
+
+export function createBlackRoom() {
+  const cameraArea = engine.addEntity()
+  Transform.create(cameraArea, { position: Vector3.create(8, 0, 8) })
+  CameraModeArea.create(cameraArea, {
+    area: Vector3.create(15, 6, 15),
+    mode: 0
+  })
+
+  createWall(Vector3.create(8, .1, 8), Quaternion.fromAngleAxis(0, Vector3.Up()))
+  createWall(
+    Vector3.create(8, 15, 8),
+    Quaternion.fromAngleAxis(0, Vector3.Up())
+  )
+  createWall(
+    Vector3.create(8, 7.5, 15.5),
+    Quaternion.fromAngleAxis(90, Vector3.Left())
+  )
+  createWall(
+    Vector3.create(8, 7.5, 15.5),
+    Quaternion.fromAngleAxis(90, Vector3.Left())
+  )
+  createWall(
+    Vector3.create(8, 7.5, 0.5),
+    Quaternion.fromAngleAxis(90, Vector3.Left())
+  )
+  createWall(
+    Vector3.create(15.5, 7.5, 8),
+    Quaternion.fromAngleAxis(90, Vector3.Forward())
+  )
+  createWall(
+    Vector3.create(0.5, 7.5, 8),
+    Quaternion.fromAngleAxis(90, Vector3.Forward())
+  )
+}
