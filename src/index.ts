1e936a3b9abd6e729ea2db175d7b7b164946c206
Seba Di Lauro <jsdilauro@gmail.com>
2023-11-27 18:52:35 -0300

    add gltf-container basic test

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@
 // import './tests/transform/index.test'
 // import './tests/camera-mode/index.test'
 // import './tests/raycast/index.test'
+// import './tests/visibility/index.test'
 
 import { createBlackRoom } from './utils/black-room'
-import './tests/visibility/index.test'
+import './tests/gltf-container/index.test'
 
 export function main(): void {
   createBlackRoom()
