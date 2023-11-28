34d50c4972642a97c9756b105fcca7ccc15e34b1
Seba Di Lauro <jsdilauro@gmail.com>
2023-11-28 14:45:03 -0300

    add basic test for material with some scenarios

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
 // import './tests/transform/index.test'
 // import './tests/camera-mode/index.test'
 // import './tests/raycast/index.test'
-
+// import './tests/visibility/index.test'
+import './tests/material/index.test'
 import { createBlackRoom } from './utils/black-room'
-import './tests/visibility/index.test'
 
 export function main(): void {
   createBlackRoom()
