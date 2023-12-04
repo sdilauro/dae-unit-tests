cc8867f762d517f59e7fc7e5d3fb90fe3ce70d51
Seba Di Lauro <jsdilauro@gmail.com>
2023-12-04 13:31:38 -0300

    add simple video-player test

--- src/tests/video-player/index.test.ts
+++ src/tests/video-player/index.test.ts
@@ -18,7 +18,7 @@ import type {
 import { waitTicks, waitTicksUntil } from '../../utils/waiters'
 import { test } from './../../testing'
 
-test('video-player 1: if exist a reference snapshot should  match with it', function* (context) {
+test('video-player: if exist a reference snapshot should match with it', function* (context) {
   yield* waitTicksUntil(() => {
     const tickNumber = EngineInfo.getOrNull(engine.RootEntity)?.tickNumber ?? 0
     if (tickNumber > 100) {
@@ -37,57 +37,9 @@ test('video-player 1: if exist a reference snapshot should  match with it', func
     scale: Vector3.create(16, 16, 1)
   })
   VideoPlayer.create(screen, {
-    src: 'https://gateway.pinata.cloud/ipfs/Qmd3eMnYQsgXjuhad1fdUCBnCQvzpoLKwRkMUrBDjedGtT/Cozumel%20Diving.mp4',
-    playing: true,
-    volume: 0.2,
-    loop: true,
-    position: 5
-  })
-
-  const videoTexture = Material.Texture.Video({ videoPlayerEntity: screen })
-
-  Material.setPbrMaterial(screen, {
-    texture: videoTexture,
-    roughness: 1.0,
-    specularIntensity: 0,
-    metallic: 0
-  })
-
-  yield* waitTicks(200)
-
-  const params: TakeAndCompareSnapshotRequest = {
-    id: 'video player 1',
-    cameraPosition: Vector3.create(8, 4, 8),
-    cameraTarget: Vector3.create(8, 4, 16),
-    snapshotFrameSize: Vector3.create(512, 512),
-    tolerance: 0.99
-  }
-
-  const result: TakeAndCompareSnapshotResponse = (
-    Testing as any
-  ).takeAndCompareSnapshot(params)
-  customAddEntity.clean()
-  if (!result.wasExist) {
-    Error(
-      'This is the first time the tool is run. The test took the reference snapshots for future testing.'
-    )
-  }
-
-  assertEquals(result.isMatch, true, `snaphot doesn't match with reference`)
-})
-
-test('video-player 2: if exist a reference snapshot should match with it', function* (context) {
-  customAddEntity.clean()
-  const screen = customAddEntity.addEntity()
-  MeshRenderer.createOrReplace(screen, {
-    mesh: { $case: 'plane', plane: { uvs: [] } }
-  })
-  Transform.create(screen, {
-    position: Vector3.create(8, 8, 15),
-    scale: Vector3.create(16, 16, 1)
-  })
-  VideoPlayer.create(screen, {
-    src: 'https://gateway.pinata.cloud/ipfs/Qmd3eMnYQsgXjuhad1fdUCBnCQvzpoLKwRkMUrBDjedGtT/Cozumel%20Diving.mp4',
+    // src: 'https://gateway.pinata.cloud/ipfs/Qmd3eMnYQsgXjuhad1fdUCBnCQvzpoLKwRkMUrBDjedGtT/Cozumel%20Diving.mp4',
+    // src: 'https://gateway.pinata.cloud/ipfs/QmTQjzjXgL5pUiJ5cbF7wkduqDEyemu4GDJasDXcwhzp73',
+    src: 'src/assets/videos/dae-video-1.mp4',
     playing: true,
     volume: 0.2,
     loop: true,
@@ -103,25 +55,65 @@ test('video-player 2: if exist a reference snapshot should match with it', funct
     metallic: 0
   })
 
-  yield* waitTicks(15)
+  let timer: number = 0.5
+  let snapshot_id: number = 0
+  let succesful_counter: number = 0
+  let test_finished: boolean = false
+  let results: TakeAndCompareSnapshotResponse[] = []
 
-  const params: TakeAndCompareSnapshotRequest = {
-    id: 'video player 2',
-    cameraPosition: Vector3.create(8, 4, 8),
-    cameraTarget: Vector3.create(8, 4, 16),
-    snapshotFrameSize: Vector3.create(512, 512),
-    tolerance: 0.99
+  yield* waitTicks(1)
+
+  const snapshots_quantity: number = 4
+
+  function takeSnapshots(dt: number) {
+    timer -= dt
+    if (timer <= 0) {
+      timer = 1
+      snapshot_id += 1
+      const params: TakeAndCompareSnapshotRequest = {
+        id: `video player ${snapshot_id}`,
+        cameraPosition: Vector3.create(8, 8, 8),
+        cameraTarget: Vector3.create(8, 8, 16),
+        snapshotFrameSize: Vector3.create(512, 512),
+        tolerance: 0.99
+      }
+
+      const result: TakeAndCompareSnapshotResponse = (
+        Testing as any
+      ).takeAndCompareSnapshot(params)
+
+      if (result) {
+        results.push(result)
+      }
+
+      if (results.length == snapshots_quantity) {
+        engine.removeSystem(takeSnapshots)
+        test_finished = true
+        return
+      }
+    }
   }
 
-  const result: TakeAndCompareSnapshotResponse = (
-    Testing as any
-  ).takeAndCompareSnapshot(params)
+  engine.addSystem(takeSnapshots)
 
-  if (!result.wasExist) {
-    Error(
-      'This is the first time the tool is run. The test took the reference snapshots for future testing.'
+  yield* waitTicksUntil(() => {
+    if (test_finished == true) {
+      return true
+    } else {
+      return false
+    }
+  })
+
+  engine.removeSystem(takeSnapshots)
+
+  for (const result of results) {
+    const snapshot_id: string = `video player  ${results.indexOf(result) + 1}`
+    assertEquals(
+      result.isMatch,
+      true,
+      `Snapshot ${snapshot_id} wasn't exist or didn't match with reference`
     )
   }
 
-  assertEquals(result.isMatch, true, `snapshot doesn't match with reference`)
+  customAddEntity.clean()
 })
